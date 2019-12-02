543af3b75f913eeaff242261e27e6263f3e02405
huanglii <849151701@qq.com>
2019-12-02 21:51:44 +0800

    update sql

--- app/service/mvt.js
+++ app/service/mvt.js
@@ -7,19 +7,21 @@ class MVTService extends Service {
   async findByBBox(tableName, layerName, bbox) {
     const sql = `
     SELECT
-      ST_AsMVT(tile, '${layerName}', 4096, 'geom') AS tiles
+      ST_AsMVT(tile, $1, 4096, 'geom') AS tiles
     FROM
       (
       SELECT
         t.gid,
-        ST_AsMVTGeom(t.geom, ST_MakeEnvelope(${bbox[0]}, ${bbox[1]}, ${bbox[2]}, ${bbox[3]}, 3857), 4096) AS geom 
+        ST_AsMVTGeom(t.geom, ST_MakeEnvelope($2, $3, $4, $5, 3857), 4096) AS geom
       FROM
       (
-        SELECT ST_Transform( geom, 3857) geom, gid FROM ${tableName}) t 
+        SELECT ST_Transform(geom, 3857) geom, gid FROM ${tableName}) t
       ) tile
     `
     const result = await this.ctx.model.query(sql, {
-      type: 'SELECT',
+      // replacements: [layerName, bbox[0], bbox[1], bbox[2], bbox[3]],
+      bind: [layerName, bbox[0], bbox[1], bbox[2], bbox[3]],
+      type: this.app.Sequelize.QueryTypes.SELECT,
     });
     return result[0].tiles;
   }
